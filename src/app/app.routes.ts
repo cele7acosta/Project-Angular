098a019481a18087f1c0d8eb25dc9f55b7de6daa
Celeste Acosta <cele7acosta@gmail.com>
2024-12-06 14:18:31 -0300

    Agregado de estilos, limpieza de código y agregado de ruta inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { EjerciciosComponent } from './ejercicios/ejercicios.component';
 import { MoviesComponent } from './movies/movies.component';
 
 export const routes: Routes = [
+    {path: '', redirectTo: 'ejercicios', pathMatch: 'full' }, // Ruta inicial redirige a 'ejercicios'
     {path: 'ejercicios', component: EjerciciosComponent},
     {path: 'pelis', component: MoviesComponent}
     
