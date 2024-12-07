290c02474088b430303cdd5b6860b9e8deadf391
Celeste Acosta <cele7acosta@gmail.com>
2024-12-07 17:08:53 -0300

    Agregado de peliculas restantes

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -16,7 +16,7 @@ export class MovieService {
       title: 'The Matrix', 
       year: 1999, 
       description: 'Un hacker descubre que la realidad es una simulación.',
-      image: "https://pics.filmaffinity.com/the_matrix-155050517-large.jpg"
+      image: "https://pics.filmaffinity.com/Matrix-840465858-large.jpg"
     },
 
     {
@@ -29,9 +29,52 @@ export class MovieService {
     {
       title: 'The Dark Knight', 
       year: 2010, 
-      description: 'Un thriller alucinante.',
+      description: 'Batman enfrenta su mayor desafío hasta el momento cuando el Joker emerge para causar estragos en Ciudad Gótica.',
       image: "https://pics.filmaffinity.com/the_dark_knight-102763119-mmed.jpg"
-    }
+    },
+
+    {
+      title: "Avengers: Endgame",
+      year: 2019,
+      description: "Los Vengadores se reúnen una última vez para deshacer el daño causado por Thanos.",
+      image: "https://pics.filmaffinity.com/avengers_endgame-433595831-large.jpg"
+    },
+
+    {
+      title: "Toy Story",
+      year: 1995,
+      description: "Un muñeco de vaquero se ve amenazado por la llegada de un nuevo juguete espacial en este clásico cuento animado.",
+      image: "https://pics.filmaffinity.com/Toy_Story-609332037-large.jpg"
+    },
+
+    {
+      title: "Jurassic Park",
+      year: 1993,
+      description: "Los dinosaurios vagan nuevamente por la Tierra en esta emocionante aventura ambientada en un parque de diversiones que salió mal.",
+      image: "https://pics.filmaffinity.com/Parque_Juraasico_Jurassic_Park-214895994-large.jpg"
+    },
+
+    {
+      title: "Forrest Gump",
+      year: 1994,
+      description: "La historia de un hombre con un coeficiente intelectual bajo que logra cosas increíbles a pesar de los desafíos de la vida.",
+      image: "https://pics.filmaffinity.com/Forrest_Gump-212765827-large.jpg"
+    },
+
+    {
+      title: "The Lion King",
+      year: 1994,
+      description: "Un joven príncipe león es expulsado de su manada por su cruel tío y debe encontrar el camino de regreso para reclamar su reino.",
+      image: "https://pics.filmaffinity.com/El_rey_leaon-585151708-large.jpg"
+    },
+
+    {
+      title: "Titanic",
+      year: 1997,
+      description: "Una historia de amor se desarrolla entre Jack y Rose a bordo del desafortunado RMS Titanic.",
+      image: "https://pics.filmaffinity.com/Titanic-816756810-large.jpg"
+    },
+
   ] 
 
   getMovies(){
